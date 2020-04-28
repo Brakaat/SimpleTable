8129b3d7335db05d50bdd64a36812e40da836893
rabiiwoof <niecheeel@gmail.com>
2020-04-28 12:02:31 +0800

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Kiri on 2020/4/28.
+//  Copyright © 2020 bararaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
